a3a2737cc09b2f0a6c3eb19aa62cd57fe74424d4
tscheys <tim.scheys@gmail.com>
2015-08-21 11:50:54 -0700

    Implement set using prototypal instantiation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,17 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return this.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  delete this[item];
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? // CONSTANT TIME LOOKUP
  */
