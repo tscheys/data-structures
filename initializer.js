7caec4e375690e8f3295e8d33b0c030282265686
tscheys <tim.scheys@gmail.com>
2015-08-22 11:20:40 -0700

    Create HTML and initializer JS files to test CPU profiles

---
+++ initializer.js
@@ -0,0 +1,15 @@
+var arr = [];
+
+function callit () {
+  for(var i = 0; i < 1000000; i++) {
+    arr.push(Queue());
+  }
+}
+callit();
+
+function enqueueAll() {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].enqueue(2);
+  }
+}
+enqueueAll();
